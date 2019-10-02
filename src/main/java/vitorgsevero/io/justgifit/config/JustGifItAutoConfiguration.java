ac16b08c5f9d0a5d877ee0f4bf4b7a17496aa167
vitorgsevero <vitorgsevero@gmail.com>
2019-10-02 16:35:34 -0300

    Writing our own custom auto configuration

--- src/main/java/vitorgsevero/io/justgifit/config/JustGifItAutoConfiguration.java
+++ src/main/java/vitorgsevero/io/justgifit/config/JustGifItAutoConfiguration.java
@@ -1,7 +1,105 @@
 package vitorgsevero.io.justgifit.config;
 
+import com.madgag.gif.fmsware.AnimatedGifEncoder;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.web.filter.FormContentFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.filter.RequestContextFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import vitorgsevero.io.justgifit.services.ConverterService;
+import vitorgsevero.io.justgifit.services.GifEncoderService;
+import vitorgsevero.io.justgifit.services.VideoDecoderService;
+
+import java.io.File;
+
 @Configuration
+@ConditionalOnClass({FFmpegFrameGrabber.class, AnimatedGifEncoder.class})
 public class JustGifItAutoConfiguration {
+
+    @Value("{$multipart.location}/gif/")
+    private String gifLocation;
+
+    @Bean
+    @ConditionalOnProperty(prefix = "com.justgifit", name = "create-result-dir")
+    public Boolean createResultDir(){
+        File gifFolder = new File(gifLocation);
+        if(!gifFolder.exists()){
+            gifFolder.mkdir();
+        }
+        return true;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(VideoDecoderService.class)
+    public VideoDecoderService videoDecoderService(){
+    return new VideoDecoderService();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(GifEncoderService.class)
+    public GifEncoderService gifDecoderService(){
+        return new GifEncoderService();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(ConverterService.class)
+    public ConverterService ConverterService(){
+        return new ConverterService();
+    }
+
+    @Configuration
+    @ConditionalOnWebApplication
+    public static class WebConfiguration{
+        @Value("{$multipart.location}/gif/")
+        private String gifLocation;
+
+        @Bean
+        @ConditionalOnProperty(prefix = "com.justgifit", name = "optimize")
+        public FilterRegistrationBean deRegisterHttpMethodFilter(HiddenHttpMethodFilter filter){
+            FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+            bean.setEnabled(false);
+            return bean;
+        }
+
+
+        @Bean
+        @ConditionalOnProperty(prefix = "com.justgifit", name = "optimize")
+        public FilterRegistrationBean deRegisterFormContentFilter(FormContentFilter filter){
+            FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+            bean.setEnabled(false);
+            return bean;
+        }
+
+        @Bean
+        @ConditionalOnProperty(prefix = "com.justgifit", name = "optimize")
+        public FilterRegistrationBean deRegisterRequestContextFilter(RequestContextFilter filter){
+            FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+            bean.setEnabled(false);
+            return bean;
+        }
+
+        @Bean
+        @ConditionalOnProperty(prefix = "com.justgifit", name = "optimize")
+        public WebMvcConfigurer webMvcConfigurer(){
+            return new WebMvcConfigurer() {
+                @Override
+                public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                    registry.addResourceHandler("/gif/**")
+                            .addResourceLocations("file:" + gifLocation);
+                }
+            };
+        }
+
+    }
+
 }
