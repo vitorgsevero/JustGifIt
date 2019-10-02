f9fb125e5c1f333bf5f9df38855d74ed5d6f5113
vitorgsevero <vitorgsevero@gmail.com>
2019-10-02 16:05:40 -0300

    Refactoring packages structure

---
+++ src/main/java/vitorgsevero/io/justgifit/config/JustGifItAutoConfiguration.java
@@ -0,0 +1,7 @@
+package vitorgsevero.io.justgifit.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JustGifItAutoConfiguration {
+}
--- src/main/java/vitorgsevero/io/justgifit/domain/Gif.java
+++
@@ -1,7 +0,0 @@
-package vitorgsevero.io.justgifit.domain;
-
-public class Gif {
-    private String gif_name;
-    private String gif_length;
-
-}
